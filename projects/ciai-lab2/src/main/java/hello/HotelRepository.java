b839116838bfc9442daeb993ced534977b73242f
Jacome Cunha <jacome@di.uminho.pt>
2015-10-08 11:00:55 +0100

    adding initial ifml for the assignment

--- projects/ciai-lab2/src/main/java/hello/HotelRepository.java
+++ projects/ciai-lab2/src/main/java/hello/HotelRepository.java
@@ -1,9 +1,15 @@
 package hello;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.*;
 
 
 public interface HotelRepository extends CrudRepository<Hotel, Long> {
+	
+	@Query("select * from hotel join address")
+	public List<Hotel> findAllWithAddress();
 
 }
 
