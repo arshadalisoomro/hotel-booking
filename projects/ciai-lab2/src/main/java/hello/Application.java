8b846f2b1bad6bf7b19718691ba700f1dc41e729
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2015-10-15 22:52:06 +0100

    Added image and comments to hotel view

--- projects/ciai-lab2/src/main/java/hello/Application.java
+++ projects/ciai-lab2/src/main/java/hello/Application.java
@@ -86,7 +86,10 @@ public class Application implements CommandLineRunner {
 											+ " could cook for us. In no time we had a delicious pasta all'amatriciana and a mixed meat dish. It was very nice.",
 											new Date()),
 									new Comment(2, "Great getaway destination with a virtually private sand beach 50m from the hotel. No need for a restaurant - Fabiana (owner)"
-											+ " cooked exquisite brazilian dishes. Very calm and tranquil place.", new Date())};
+											+ " cooked exquisite brazilian dishes. Very calm and tranquil place.", new Date()),
+									new Comment(3, "The hosts are simply amazing and constantly go an extra mile in their efforts to make you feel welcome. Genuine family atmosphere!"
+											+ " The hotel is rated as a three-star establishment, but the hosts, deserve seven stars. :)", new Date())
+		};
 
 		Hotel intercontinental = myHotels[0];
 
