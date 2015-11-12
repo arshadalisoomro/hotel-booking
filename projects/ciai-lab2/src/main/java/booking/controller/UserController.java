36f57e6152f5ee5c9d77d2f1636b8487474aa526
João Pinto <jfd.pinto@campus.fct.unl.pt>
2015-11-12 14:49:30 +0000

    Sign up fully operational & name of current user in header corrected

--- projects/ciai-lab2/src/main/java/booking/controller/UserController.java
+++ projects/ciai-lab2/src/main/java/booking/controller/UserController.java
@@ -5,8 +5,11 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import booking.model.Authority;
 import booking.model.Booking;
 import booking.model.CustomUserDetail;
+import booking.model.CustomUserDetailsService;
 import booking.model.User;
 import booking.repository.AuthorityRepository;
 import booking.repository.BookingRepository;
@@ -30,104 +34,119 @@ public class UserController {
 
 	@Autowired
 	UserRepository users;
-	
+
 	@Autowired
 	BookingRepository bookings;
-	
+
 	@Autowired
 	AuthorityRepository authorities;
-	
+
+	@Autowired 
+	private AuthenticationManager authMgr;
+
+	@Autowired 
+	private CustomUserDetailsService customUserDetailsSvc;
+
 	// GET  /users 			- the list of users
-    @RequestMapping(method=RequestMethod.GET)
-    public String index(Model model) {
-        model.addAttribute("users", users.findAll());
-        return "users/index";
-    }
-    
-    // GET  /users/new			- the form to fill the data for a new user
-    @RequestMapping(value="/new", method=RequestMethod.GET)
-    public String newHotel(Model model) {
-    	model.addAttribute("user", new User());
-    	return "users/create";
-    }
-    
-    // POST /users         	- creates a new user
-    @RequestMapping(method=RequestMethod.POST)
-    public String saveIt(@ModelAttribute User user, Model model)
-    {
-    	Authority authority = authorities.findByRole("ROLE_USER");
-    	user.setAuthority(authority);
-    	users.save(user);
-    	model.addAttribute("user", user);
-    	return "redirect:/users";
-    }
-    
-    // GET /login
-    @RequestMapping(value="/login", method=RequestMethod.GET)
-    public String login(Model model) {
-    	return "index";
-    }
-    
-    // GET  /users/{id} 		- the user with identifier {id}
-    @RequestMapping(value="{id}", method=RequestMethod.GET) 
-    public String show(@PathVariable("id") long id, Model model) {
-    	User user = users.findOne(id);
-    	if( user == null )
-    		throw new HotelNotFoundException();    	
-    	model.addAttribute("user", user);    	    	    	
-    	return "users/show";
-    }
-    
-    @RequestMapping(value="/me", method=RequestMethod.GET)
-    public String showActiveProfile(Model model) throws HotelNotFoundException
-    {
-    	Authentication auth = SecurityContextHolder.getContext().getAuthentication();    	    
-    	CustomUserDetail myUser= (CustomUserDetail) auth.getPrincipal();       	
-    	model.addAttribute("bookings", getUserBookings(myUser.getUser().getId()));    	    	
-        model.addAttribute("user", myUser.getUser());
+	@RequestMapping(method=RequestMethod.GET)
+	public String index(Model model) {
+		model.addAttribute("users", users.findAll());
+		return "users/index";
+	}
+
+	// GET  /users/new			- the form to fill the data for a new user
+	@RequestMapping(value="/new", method=RequestMethod.GET)
+	public String newHotel(Model model) {
+		model.addAttribute("user", new User());
+		return "users/create";
+	}
+
+	// POST /users         	- creates a new user
+	@RequestMapping(method=RequestMethod.POST)
+	public String saveIt(@ModelAttribute User user, Model model)
+	{
+		try{
+			Authority authority = authorities.findByRole("ROLE_USER");
+			user.setAuthority(authority);
+			users.save(user);
+			UserDetails userDetails = customUserDetailsSvc.loadUserByUsername(user.getUsername());
+			UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, user.getPassword(), userDetails.getAuthorities());
+			authMgr.authenticate(auth);
+			SecurityContextHolder.getContext().setAuthentication(auth);
+			return "redirect:/users/me";
+			 
+		}
+		catch(Exception e){
+			return "error";
+		}
+	}
+
+	// GET /login
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String login(Model model) {
+		return "index";
+	}
+
+	// GET  /users/{id} 		- the user with identifier {id}
+	@RequestMapping(value="{id}", method=RequestMethod.GET) 
+	public String show(@PathVariable("id") long id, Model model) {
+		User user = users.findOne(id);
+		if( user == null )
+			throw new HotelNotFoundException();    	
+		model.addAttribute("user", user);    	    	    	
 		return "users/show";
-    }
-    
-    public Iterable<Booking> getUserBookings(long user_id)
-    {
-    	Iterator<Booking> itbookings = bookings.findAll().iterator();
-    	List<Booking> bookingsList = new ArrayList<Booking>();
-    	
-    	while(itbookings.hasNext())
-    	{
-    		Booking b = itbookings.next();
-    		if(b.getUser().getId() == user_id)
-    			bookingsList.add(b);
-    	}
-    	
-    	return bookingsList;
-    }    
-    
-    // GET  /users/{id}/remove 	- removes the user with identifier {id}
-    @RequestMapping(value="{id}/remove", method=RequestMethod.GET)
-    public String remove(@PathVariable("id") long id, Model model) {
-    	User user = users.findOne(id);    	
-    	if( user == null )
-    		throw new UserNotFoundException();    	
-    	users.delete(user);
-    	model.addAttribute("users", users.findAll());
-    	return "users/index";
-    }  
-    
-    // GET /users/{id}/edit - form to edit user
-    @RequestMapping(value="{id}/edit", method=RequestMethod.GET)
-    public String edit(@PathVariable("id") long id, Model model) { 	
-    	User user = users.findOne(id);
-    	model.addAttribute("user", user);    	
-    	return "users/edit";
-    }
-    
-    // POST /users/{id}        	- edit a user
-    @RequestMapping(value="/{id}", method=RequestMethod.POST)
-    public String edit(@PathVariable("id") long id, @ModelAttribute User user, Model model)
-    {
-    	users.save(user);
-    	model.addAttribute("user", user);
-    	return "redirect:/users/";
-    }	
+	}
+
+	@RequestMapping(value="/me", method=RequestMethod.GET)
+	public String showActiveProfile(Model model) throws HotelNotFoundException
+	{
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();    	    
+		CustomUserDetail myUser= (CustomUserDetail) auth.getPrincipal();       	
+		model.addAttribute("bookings", getUserBookings(myUser.getUser().getId()));    	    	
+		model.addAttribute("user", myUser.getUser());
+		return "users/show";
+	}
+
+	public Iterable<Booking> getUserBookings(long user_id)
+	{
+		Iterator<Booking> itbookings = bookings.findAll().iterator();
+		List<Booking> bookingsList = new ArrayList<Booking>();
+
+		while(itbookings.hasNext())
+		{
+			Booking b = itbookings.next();
+			if(b.getUser().getId() == user_id)
+				bookingsList.add(b);
+		}
+
+		return bookingsList;
+	}    
+
+	// GET  /users/{id}/remove 	- removes the user with identifier {id}
+	@RequestMapping(value="{id}/remove", method=RequestMethod.GET)
+	public String remove(@PathVariable("id") long id, Model model) {
+		User user = users.findOne(id);    	
+		if( user == null )
+			throw new UserNotFoundException();    	
+		users.delete(user);
+		model.addAttribute("users", users.findAll());
+		return "users/index";
+	}  
+
+	// GET /users/{id}/edit - form to edit user
+	@RequestMapping(value="{id}/edit", method=RequestMethod.GET)
+	public String edit(@PathVariable("id") long id, Model model) { 	
+		User user = users.findOne(id);
+		model.addAttribute("user", user);    	
+		return "users/edit";
+	}
+
+	// POST /users/{id}        	- edit a user
+	@RequestMapping(value="/{id}", method=RequestMethod.POST)
+	public String edit(@PathVariable("id") long id, @ModelAttribute User user, Model model)
+	{
+		users.save(user);
+		model.addAttribute("user", user);
+		return "redirect:/users/";
+	}	
 }
